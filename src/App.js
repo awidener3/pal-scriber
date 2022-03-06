808da674e1a010511246a9b7640a9598ce26e78e
Alex Widener <awidener3@gmail.com>
2022-03-06 15:23:37 -0500

    added margin to top and bottom of app for mobile

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import InputListener from './InputListener';
 
 function App() {
 	return (
-		<div className="App d-flex flex-column justify-content-center align-items-center vh-100">
+		<div className="App d-flex flex-column justify-content-center align-items-center my-5 vh-100">
 			<InputListener />
 		</div>
 	);
